b0760835f7cf08fe69f812aa8a6845e74fa53d0a
Genaro Martinez <genaromartinez@Genaros-MacBook-Pro.local>
2019-02-23 16:22:25 -0700

    faster time

--- static/js/script.js
+++ static/js/script.js
@@ -99,7 +99,7 @@ function revealCard(event) {
                 hideACard(cardsPicked[1]);
                 cardsPicked = [];
             }
-            window.setTimeout(hidePickedCards, 1000);
+            window.setTimeout(hidePickedCards, 500);
         }
     }
 }
